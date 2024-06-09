3fbb10caa94000454bd4bf06f753af2b131f17b0
André Fukuda <andre.alex.fukuda@gmail.com>
2024-06-08 23:23:43 -0300

    Promotion(Pawn)

--- src/application/Program.java
+++ src/application/Program.java
@@ -45,6 +45,19 @@ public class Program {
 					captured.add(capturedPiece);
 				}
 				
+				if(chessMatch.getPromoted() != null) {
+					
+					System.out.print("Enter piece for promotion [B/N/R/Q]: ");
+					String type = sc.nextLine().toUpperCase();
+					
+					while(!type.equals("Q") && !type.equals("N") && !type.equals("B") && !type.equals("R")) {
+						System.out.print("Invalid piece, try again: ");
+						type = sc.nextLine().toUpperCase();
+					}
+					
+					chessMatch.replacePromotedPiece(type);
+				}
+				
 			} catch (ChessException e){
 				
 				System.out.println(e.getMessage());
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,5 +1,4 @@
 package chess;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,6 +21,7 @@ public class ChessMatch {
 	private boolean check;//inicia com false
 	private boolean checkMate;
 	private ChessPiece enPassantVulnerable;
+	private ChessPiece promoted;
 	
 	private List<Piece> piecesOnTheBoard = new ArrayList<>();
 	private List<Piece> capturedPieces = new ArrayList<>();
@@ -51,6 +51,10 @@ public class ChessMatch {
 	
 	public ChessPiece getEnPassantVulnerable() {
 		return enPassantVulnerable;
+	}
+	
+	public ChessPiece getPromoted() {
+		return promoted;
 	}
 
 	public ChessPiece[][]getPieces(){
@@ -92,6 +96,17 @@ public class ChessMatch {
 		
 		ChessPiece movedPiece = (ChessPiece)board.piece(target);
 		
+		//specialMove Promotion
+		promoted = null;
+		if(movedPiece instanceof Pawn) {
+			if(movedPiece.getColor() == Color.WHITE && target.getRow() == 0 || movedPiece.getColor() == Color.BLACK && target.getRow() == 7) {
+				
+				promoted = (ChessPiece)board.piece(target);
+				promoted = replacePromotedPiece("Q");
+				
+			}
+		}
+		
 		check = (testCheck(opponent(currentPlayer)))? true : false;//se o opponente entrou em check, retorna verdadeiro, caso contrário, falso
 		
 		if(testCheckMate(opponent(currentPlayer))) {//se o oponente da peça que mexeu, ficou em checkmate:
@@ -110,6 +125,38 @@ public class ChessMatch {
 		}
 		
 		return (ChessPiece)capturedPiece;//downcasting to chessPiece (it was piece)
+	}
+	
+	public ChessPiece replacePromotedPiece(String type) {
+		if(promoted == null) {
+			
+			throw new IllegalStateException("There is no piece to be replaced");
+		}
+		if(!type.equals("B") && !type.equals("Q") && !type.equals("R") && !type.equals("N")) {
+			
+			return promoted;
+		}
+		
+		Position pos = promoted.getChessPosition().toPosition();
+		Piece p = board.removePiece(pos);
+		piecesOnTheBoard.remove(p);
+		
+
+		ChessPiece newPiece = newPiece(type, promoted.getColor());
+		board.placePiece(newPiece, pos);
+		piecesOnTheBoard.add(newPiece);
+		
+		return newPiece;
+		
+	}
+	
+	private ChessPiece newPiece(String type, Color color) {
+		
+		if(type.equals("B")) return new Bishop(board,color);
+		if(type.equals("K")) return new Knight(board,color);
+		if(type.equals("Q")) return new Queen(board,color);
+		//if(type.equals("R")) return new Rook(board,color); não precisa pq se não for B,K ou Q,será Rook
+		return new Rook(board,color);
 	}
 	
 	private void validateSourcePosition(Position position) {
